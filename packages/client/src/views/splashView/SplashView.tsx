81dd632aea4721e022f0872c7491891c77909f56
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2025-05-01 18:57:53 -0300

    I want to be forever young

--- packages/client/src/views/splashView/SplashView.tsx
+++ packages/client/src/views/splashView/SplashView.tsx
@@ -10,22 +10,31 @@ type SVProps = {
 export default function SplashView({ onComplete }: SVProps) {
   const [playerName, setPlayerName] = useState<string>('');
   const [playerColor, setPlayerColor] = useState<string>(getRandomColor());
+  const [error, setError] = useState<string | undefined>();
+
+  const handleGameStart = (isCpuGame: boolean) => {
+    if (playerName.length === 0) {
+      setError('Name is required...');
+    } else {
+      onComplete(playerName, playerColor, isCpuGame);
+    }
+  };
 
   return (
     <div className="splash-view-container">
       <GroupBox>
         <div className="splash-view-items">
-          {' '}
           <h1>Battle Snakes</h1>
           <GroupBox label="Name">
             <TextInput placeholder="Enter your name" onChange={(e) => setPlayerName(e.target.value)} />
+            {error && <span style={{ color: 'red' }}>{error}</span>}
           </GroupBox>
-          <span className={'color-input'}>
+          <span className="color-input">
             Select snake color: {'  '}
             <ColorInput defaultValue={playerColor} onChange={(e) => setPlayerColor(e.target.value)} />
           </span>
-          <Button onClick={() => onComplete(playerName, playerColor, false)}>Play Online</Button>
-          <Button onClick={() => onComplete(playerName, playerColor, true)}>Play Against CPU</Button>
+          <Button onClick={() => handleGameStart(false)}>Play Online</Button>
+          <Button onClick={() => handleGameStart(true)}>Play Against CPU</Button>
         </div>
       </GroupBox>
     </div>
