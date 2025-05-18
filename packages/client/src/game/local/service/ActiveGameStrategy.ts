a2b61516377a074bf8df92e080adce4b5bb4e2bf
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-05-18 20:39:25 -0300

    sneed.

--- packages/client/src/game/local/service/ActiveGameStrategy.ts
+++ packages/client/src/game/local/service/ActiveGameStrategy.ts
@@ -1,16 +1,24 @@
 import { Collision, CollisionService, DEFAULT_FOOD_COUNT, GameLoop, GameState, SpawnService } from '@battle-snakes/shared';
 import { LocalGameContext } from '../LocalGame';
+import { InputBuffer } from './InputBuffer';
 
 export class ActiveGameStrategy {
+  private localPlayerId;
+
   private gameState: GameState;
+  private inputBuffer: InputBuffer;
 
   private spawnService: SpawnService;
 
   private movementAccumulator = 0;
 
-  constructor({ gameState, spawnService }: LocalGameContext) {
+  constructor({ gameState, spawnService, gameConfigOptions }: LocalGameContext) {
+    this.localPlayerId = gameConfigOptions.playerName;
+
     this.gameState = gameState;
     this.spawnService = spawnService;
+
+    this.inputBuffer = new InputBuffer(gameState);
   }
 
   tick(deltaTime: number) {
@@ -18,13 +26,13 @@ export class ActiveGameStrategy {
     // The speed of each snake is tied to the update interval.
     this.movementAccumulator += deltaTime;
     if (this.movementAccumulator < GameLoop.GAME_STATE_UPDATE_INTERVAL_MS) {
-      console.log('movementAccumulator', this.movementAccumulator);
       return;
     } else {
       this.movementAccumulator -= GameLoop.GAME_STATE_UPDATE_INTERVAL_MS;
     }
 
     // Step One: process input
+    this.handleInput();
 
     // Step Two: update player positions
     this.movementTick();
@@ -74,6 +82,17 @@ export class ActiveGameStrategy {
     }
 
     if (wasScoreUpdated) {
+    }
+  }
+
+  private handleInput() {
+    const input = this.inputBuffer.getNextInput();
+
+    for (const player of this.gameState.getActivePlayers()) {
+      if (input && player.getPlayerId() === this.localPlayerId) {
+        player.setDirection(input);
+      }
+      // CPU players.
     }
   }
 }
