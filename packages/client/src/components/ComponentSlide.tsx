c7cbafbe855604904d229254342458ed15b68433
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-10-05 23:44:30 -0300

    redo it.

--- packages/client/src/components/ComponentSlide.tsx
+++ packages/client/src/components/ComponentSlide.tsx
@@ -1,12 +1,5 @@
 import { useWindowSize } from '@/hooks/useWindowSize';
 import { useEffect, useState } from 'react';
-import { Button } from 'react95';
-
-// Open:Close labels
-const Labels = {
-  left: ['<', '>'],
-  right: ['>', '<'],
-};
 
 type ComponentSlideProps = {
   children: React.ReactNode;
@@ -30,33 +23,10 @@ export const ComponentSlide = ({ children, position }: ComponentSlideProps) => {
   // Large screens: inline render
   if (!shouldDisplay) return <>{children}</>;
 
-  const slideClasses = {
-    left: {
-      open: 'translate-x-0',
-      closed: '-translate-x-[calc(100%-30px)]',
-    },
-    right: {
-      open: 'translate-x-0',
-      closed: 'translate-x-[calc(100%-25px)]',
-    },
-  };
-
   return (
-    <div className={`absolute top-1/5 ${position}-0 w-fit overflow-hidden z-0 pointer-events-none`}>
-      {/* Panel */}
-      <div
-        className={`flex flex-row ${position === 'right' ? 'flex-row-reverse' : ''} ${
-          isOpen ? slideClasses[position].open : slideClasses[position].closed
-        }`}>
-        <div className={isOpen ? 'pointer-events-auto' : 'opacity-0 pointer-events-none'}>{children}</div>
-        <Button
-          type="button"
-          onClick={() => {
-            setIsOpen((v) => !v);
-          }}
-          className="!w-[40px] !z-40 pointer-events-auto">
-          {isOpen ? Labels[position][0] : Labels[position][1]}
-        </Button>
+    <div>
+      <div>
+        <div>{children}</div>
       </div>
     </div>
   );
